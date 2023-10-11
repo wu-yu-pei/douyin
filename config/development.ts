aa6d30eb118e0bedeb7fd659d49fc15ef5784659
wuyupei <495174699@qq.com>
2023-10-11 15:28:20 +0800

    db

--- config/development.ts
+++ config/development.ts
@@ -6,6 +6,20 @@ const devConfig: any = {
   port: 8888,
   enableReplay: false,
   secret: 'WUYUPEI',
+  mysqlConfig: {
+    host: '8.219.66.21',
+    port: 3306,
+    username: 'root',
+    password: '123456',
+    database: 'douyin',
+    dialect: 'mysql',
+    logging: false,
+  },
+  redisConfig: {
+    port: 6379,
+    host: '8.219.66.21',
+    db: 2,
+  },
 };
 
 _.merge(devConfig, defaultConfig);
